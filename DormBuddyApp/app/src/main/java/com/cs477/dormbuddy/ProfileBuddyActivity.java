e8b4610d2606936e58879319440d9376beee1448
Nabil Darwich <Nabil.Darwich@ellucian.com>
2017-11-17 23:06:42 -0500

    EVEN BIGGER UPDATE: complete credentials, complete profile buddy, add easter egg buddy

--- DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/ProfileBuddyActivity.java
+++ DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/ProfileBuddyActivity.java
@@ -1,20 +1,126 @@
 package com.cs477.dormbuddy;
 
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static com.cs477.dormbuddy.LocalUserHelper.BUILDING_ID;
+import static com.cs477.dormbuddy.LocalUserHelper.BUILDING_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper.FULL_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper.ICON;
+import static com.cs477.dormbuddy.LocalUserHelper.LOGGED_IN;
+import static com.cs477.dormbuddy.LocalUserHelper.ROOM_NUMBER;
+import static com.cs477.dormbuddy.LocalUserHelper.TABLE_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper._ID;
 
 public class ProfileBuddyActivity extends AppCompatActivity {
+    TextView nameView, gNumberView, buildingNameView, roomNumberView;
+    ImageView img; //user image
+    private SQLiteDatabase db = null;
+    private LocalUserHelper dbHelper = null;
+    private Cursor mCursor;
+    private String storedGNumber;
+    final static String[] columns = { _ID, FULL_NAME, LOGGED_IN, BUILDING_ID, BUILDING_NAME, ROOM_NUMBER, ICON };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_buddy);
+        nameView = findViewById(R.id.nameText);
+        gNumberView = findViewById(R.id.gNumberText);
+        buildingNameView = findViewById(R.id.buildingText);
+        roomNumberView = findViewById(R.id.roomText);
+        dbHelper = new LocalUserHelper(this);
+        db = dbHelper.getWritableDatabase();
+        mCursor = db.query(TABLE_NAME, columns, null, new String[] {}, null, null,
+                null);
+        try { //since user is logged in, just gobble up his user data
+            mCursor.moveToPosition(0);
+            storedGNumber = "" + mCursor.getInt(0);
+            String gNumber = "G" + storedGNumber;
+            gNumberView.setText(gNumber);
+            nameView.setText(mCursor.getString(1));
+            buildingNameView.setText(mCursor.getString(4));
+            roomNumberView.setText(mCursor.getString(5));
+            byte[] userImage = mCursor.getBlob(6);
+            if (userImage.length > 0) {
+                displayImage(userImage); //image exists -- display it
+            }
+            mCursor.close();
+        } catch (Exception e) {
+            startActivity(new Intent(this, CredentialsActivity.class));
+        }
     }
 
     public void changeHousingClicked(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, RegisterActivity.class));
+        finish();
+    }
+
+    public void uploadImage(View view) {
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), 3);
+    }
+
+    //user intentionally logging out just updates the logged in column
+    public void logoutClicked(View view) {
+        ContentValues cv = new ContentValues(1);
+        cv.put(LOGGED_IN, 0); //gives illusion of being logged out but user info is still in table
+        db.update(TABLE_NAME, cv, _ID + "=" + storedGNumber, null);
+        db.close();
+        mCursor.close();
+        startActivity(new Intent(this, CredentialsActivity.class));
+        finish();
+    }
+
+
+
+    public void displayImage(byte[] imageBytes) {
+        img = findViewById(R.id.userImage);
+        Bitmap bmp = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        img.setImageBitmap(bmp);
+    }
+
+    //user uploaded image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==3 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bmp = null;
+            try {
+                bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                bmp = Bitmap.createScaledBitmap(bmp, 400,400,true); //makes bitmap tiny to not abuse memory
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.PNG, 10, stream); //bmp gets compressed to the stream
+                ContentValues cv = new ContentValues(1);
+                byte[] byteArray = stream.toByteArray(); //stream becomes a byte array
+                cv.put(ICON, byteArray); //updates stored byte array for user local table
+                db.update(TABLE_NAME, cv, _ID + "=" + storedGNumber, null); //uploads image
+                displayImage(byteArray); //displays on screen
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/RegisterActivity.java
+++ DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/RegisterActivity.java
@@ -1,18 +1,130 @@
 package com.cs477.dormbuddy;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static com.cs477.dormbuddy.LocalUserHelper.BUILDING_ID;
+import static com.cs477.dormbuddy.LocalUserHelper.BUILDING_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper.FULL_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper.ICON;
+import static com.cs477.dormbuddy.LocalUserHelper.LOGGED_IN;
+import static com.cs477.dormbuddy.LocalUserHelper.ROOM_NUMBER;
+import static com.cs477.dormbuddy.LocalUserHelper.TABLE_NAME;
+import static com.cs477.dormbuddy.LocalUserHelper._ID;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    public void onRegisterClicked(View view) {
-
-    }
+    EditText roomNumberView;
+    Spinner buildingSpinnerView;
+    Button registerOrUpdateButton;
+    private SQLiteDatabase db = null;
+    private LocalUserHelper dbHelper = null;
+    private Cursor mCursor;
+    ArrayAdapter<CharSequence> mAdapter;
+    final static String[] columns = { _ID, FULL_NAME, LOGGED_IN, BUILDING_ID, BUILDING_NAME, ROOM_NUMBER, ICON };
+    String storedGNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //check if user is logged in first, redirect them to log in if not
+        dbHelper = new LocalUserHelper(this);
+        db = dbHelper.getWritableDatabase();
+        roomNumberView = findViewById(R.id.roomNumber);
+        buildingSpinnerView = findViewById(R.id.buildingSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        mAdapter = ArrayAdapter.createFromResource(this, R.array.GMU_houses, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        buildingSpinnerView.setAdapter(mAdapter);
+        mCursor = db.query(TABLE_NAME, columns, null, new String[] {}, null, null,
+                null); //to check if user exists
+        registerOrUpdateButton = findViewById(R.id.registerOrUpdate);
+        //try to get the user
+        try { //if try is successful, changes register button to update button
+            mCursor.moveToPosition(0);
+            storedGNumber = "" + mCursor.getInt(0); //gets the g number
+            int isLoggedIn = mCursor.getInt(2);
+            if (isLoggedIn == 1) { //user is not logged in but exists => he clicked change room
+                registerOrUpdateButton.setText("Update");
+                registerOrUpdateButton.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                registerOrUpdateButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        if (inputIsCorrect())
+                            onUpdateClicked();
+                    }
+                });
+            } else { //user exists but is not logged in => user becomes logged in
+                loginUser();
+            }
+        } catch (Exception e) { //user not in the system yet, so proceed without modifying buttons
+            registerOrUpdateButton.setText("Register");
+            registerOrUpdateButton.setBackgroundColor(getResources().getColor(R.color.colorAmber));
+            registerOrUpdateButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (inputIsCorrect())
+                        onRegisterClicked();
+                }
+            });
+        }
+    }
+
+    private Boolean inputIsCorrect(){
+        if (roomNumberView.getText().toString().equals("")) { //checks that room number is not empty
+            Toast.makeText(this, "Enter a room number", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    //creates new suer
+    public void onRegisterClicked() {
+        ContentValues cv = new ContentValues(7);
+        cv.put(_ID, 123456); //g number
+        cv.put(FULL_NAME, "John Doe");
+        cv.put(LOGGED_IN, 1);
+
+        cv.put(BUILDING_ID, buildingSpinnerView.getSelectedItemPosition());
+        cv.put(BUILDING_NAME, buildingSpinnerView.getSelectedItem().toString());
+        cv.put(ROOM_NUMBER, roomNumberView.getText().toString());
+        cv.put(ICON, new byte[]{}); //icon is just an empty byte array to start
+        db.insert(TABLE_NAME, null, cv);
+        db.close();
+        mCursor.close();
+        finish();
+    }
+
+    //modifies existing user data
+    public void onUpdateClicked() {
+        ContentValues cv = new ContentValues(3);
+        cv.put(BUILDING_ID, buildingSpinnerView.getSelectedItemPosition());
+        cv.put(BUILDING_NAME, buildingSpinnerView.getSelectedItem().toString());
+        cv.put(ROOM_NUMBER, roomNumberView.getText().toString());
+        //updates the 3 fields
+        db.update(TABLE_NAME, cv, _ID + "=" + storedGNumber, null);
+        db.close();
+        mCursor.close();
+        finish();
+    }
+
+    public void loginUser() {
+        ContentValues cv = new ContentValues(1);
+        cv.put(LOGGED_IN, 1);
+        //marks user as logged in
+        db.update(TABLE_NAME, cv, _ID + "=" + storedGNumber, null);
+        db.close();
+        mCursor.close();
+        finish();
     }
 }
