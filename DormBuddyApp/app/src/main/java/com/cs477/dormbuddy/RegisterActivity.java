3815d3132fadfcc6af943d8151edd62d4322df17
Nabil Darwich <Nabil.Darwich@ellucian.com>
2017-11-20 17:50:24 -0500

    fix UI

--- DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/RegisterActivity.java
+++ DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/RegisterActivity.java
@@ -43,7 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         roomNumberView = findViewById(R.id.roomNumber);
         buildingSpinnerView = findViewById(R.id.buildingSpinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        mAdapter = ArrayAdapter.createFromResource(this, R.array.GMU_houses, android.R.layout.simple_spinner_item);
+        mAdapter = ArrayAdapter.createFromResource(this, R.array.GMU_houses, R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
