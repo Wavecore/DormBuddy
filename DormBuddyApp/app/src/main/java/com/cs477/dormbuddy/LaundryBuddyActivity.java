c14f7d2cb794441e83eb2cf3cafb6c1cb7e2e844
Nabil Darwich <Nabil.Darwich@ellucian.com>
2017-11-22 17:24:46 -0500

    slightly better laundry buddy

--- DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/LaundryBuddyActivity.java
+++ DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/LaundryBuddyActivity.java
@@ -3,6 +3,7 @@ package com.cs477.dormbuddy;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +15,17 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class LaundryBuddyActivity extends AppCompatActivity {
     LaundryAdapter washerAdapter;
     LaundryAdapter dryerAdapter;
-    ListView laundryList;
-    ListView dryerList;
+    RecyclerView laundryList;
+    RecyclerView dryerList;
+    ArrayList<String> washers = new ArrayList<String>();
+    ArrayList<String> dryers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +33,25 @@ public class LaundryBuddyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_laundry_buddy);
         laundryList = findViewById(R.id.washers);
         dryerList = findViewById(R.id.dryers);
-        washerAdapter = new LaundryAdapter(this, true);
-        dryerAdapter = new LaundryAdapter(this, false);
-        washerAdapter.addItem("HI");
-        washerAdapter.addItem("HI Number 2");
-        washerAdapter.addItem("HI3");
-        dryerAdapter.addItem("hello");
-        dryerAdapter.addItem("sup");
+        laundryList.setHasFixedSize(true);
+        dryerList.setHasFixedSize(true);
+        LinearLayoutManager horizontalLayout = new LinearLayoutManager(this);
+        horizontalLayout.setOrientation(LinearLayoutManager.HORIZONTAL);
+        LinearLayoutManager horizontalLayout2 = new LinearLayoutManager(this);
+        horizontalLayout2.setOrientation(LinearLayoutManager.HORIZONTAL);
+        /////////fill arraylists here///////
+        washers.add("HI");
+        washers.add("HI Number 2");
+        washers.add("HI3");
+        dryers.add("hello");
+        dryers.add("sup");
+        ///////////////////////////////////
+        washerAdapter = new LaundryAdapter(washers, true);
+        dryerAdapter = new LaundryAdapter(dryers, false);
         laundryList.setAdapter(washerAdapter);
         dryerList.setAdapter(dryerAdapter);
+        laundryList.setLayoutManager(horizontalLayout);
+        dryerList.setLayoutManager(horizontalLayout2);
     }
 
 
@@ -46,45 +60,51 @@ public class LaundryBuddyActivity extends AppCompatActivity {
 
     }
 
-    private class LaundryAdapter extends BaseAdapter {
-        private ArrayList<String> machineNames = new ArrayList<String>();
-        Context context;
-        LayoutInflater mInflater;
-        boolean isWasher;
-        public LaundryAdapter(Context applicationContext, boolean isWasher) {
-            this.context = context;
+    public class LaundryHolder extends RecyclerView.ViewHolder {
+        public TextView machineName;
+
+        public LaundryHolder(View v) {
+            super(v);
+            machineName = (TextView) v.findViewById(R.id.machineName);
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(LaundryBuddyActivity.this, "Laundry item clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public class LaundryAdapter extends RecyclerView.Adapter<LaundryHolder> {
+        private ArrayList<String> machineNames;
+        private boolean isWasher;
+
+        public LaundryAdapter(ArrayList<String> data, boolean isWasher) {
+            machineNames = data;
             this.isWasher = isWasher;
-            mInflater = (LayoutInflater.from(applicationContext));
-        }
-        public void addItem(final String item) {
-            machineNames.add(item);
-            notifyDataSetChanged();
-        }
-        public void removeItem(final String item) {
-            machineNames.remove(item);
-            notifyDataSetChanged();
-        }
-        public int getCount() {
-            return machineNames.size();
-        }
-        public String getItem(int position) {
-            return machineNames.get(position);
-        }
-        public long getItemId(int position) {
-            return position;
         }
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        public LaundryHolder onCreateViewHolder(ViewGroup parent,int viewType) {
             int layout;
             if (isWasher) {
                 layout = R.layout.washer_item;
             } else {
                 layout = R.layout.dryer_item;
             }
-            view = mInflater.inflate(layout, null);
-            TextView name = view.findViewById(R.id.machineName);
-            name.setText(machineNames.get(i));
-            return view;
+            // create a new view
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(layout, parent, false);
+            LaundryHolder holder = new LaundryHolder(view);
+            return holder;
+        }
+        @Override
+        public void onBindViewHolder(final LaundryHolder holder, int position) {
+            holder.machineName.setText(machineNames.get(position));
+
+        }
+        @Override
+        public int getItemCount() {
+            return machineNames.size();
         }
     }
 }
