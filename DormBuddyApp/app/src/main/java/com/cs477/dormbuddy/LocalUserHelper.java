dcb217facfa7c0c2d0584f69d1182a66336ea9b7
Vincent Liu <liuvincent19915@gmail.com>
2017-11-26 19:09:55 -0500

    Added database for reservations

--- DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/LocalUserHelper.java
+++ DormBuddyApp/app/src/main/java/com/cs477/dormbuddy/LocalUserHelper.java
@@ -1,6 +1,7 @@
 package com.cs477.dormbuddy;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 //database helper that keeps user logged in and displays their templates
 public class LocalUserHelper extends SQLiteOpenHelper {
-    final static private Integer VERSION = 57;
+    final static private Integer VERSION = 59;
     //===============Buildings Table=====================
     final static String TABLE_BUILDING = "dormbuddy_building";
     final static String BUILDING_ID = "building_id";
@@ -60,7 +61,7 @@ public class LocalUserHelper extends SQLiteOpenHelper {
 
 
 
-    final private String[] TABLES = {TABLE_BUILDING,TABLE_BUILDING_MAPS,TABLE_ROOM, TABLE_RESERVATION,TABLE_USER};
+    final private String[] TABLES = {TABLE_BUILDING,TABLE_BUILDING_MAPS,TABLE_ROOM,TABLE_USER,TABLE_RESERVATION};
 
     final Context context;
     /*
@@ -115,10 +116,23 @@ public class LocalUserHelper extends SQLiteOpenHelper {
                     ROOM_MAX_OCCUPANCY+" INTEGER CHECK("+ROOM_MAX_OCCUPANCY+" >= 0),"+
                     "PRIMARY KEY ("+BUILDING_ID+", "+ROOM_NUMBER+"), "+
                     "FOREIGN KEY ("+BUILDING_ID+") REFERENCES "+TABLE_BUILDING+" ("+BUILDING_ID+") ON DELETE SET NULL DEFERRABLE);";
-    final private static String CREATE_RESERVATION ="";
+    final private  String CREATE_RESERVATION =
+            "CREATE TABLE "+TABLE_RESERVATION+" ("+
+                    BUILDING_ID +" INTEGER, "+
+                    ROOM_NUMBER+" INTEGER NOT NULL, "+
+                    USER_ID+ " INTEGER, "+
+                    RESERVATION_TITLE+ " TEXT, "+
+                    RESERVATION_DESCRIPTION+" TEXT, "+
+                    RESERVATION_ICON+ " BLOB, "+
+                    RESERVATION_START_TIME+ " INTEGER NOT NULL, "+
+                    RESERVATION_END_TIME+ " INTEGER NOT NULL, "+
+                    "PRIMARY KEY ("+BUILDING_ID+", "+ROOM_NUMBER+","+RESERVATION_START_TIME+","+RESERVATION_END_TIME+"), "+
+                    "FOREIGN KEY ("+BUILDING_ID+") REFERENCES "+TABLE_ROOM+" ("+BUILDING_ID+") ON DELETE SET NULL DEFERRABLE,"+
+                    "FOREIGN KEY ("+ROOM_NUMBER+") REFERENCES "+TABLE_ROOM+" ("+ROOM_NUMBER+") ON DELETE SET NULL DEFERRABLE,"+
+                    "FOREIGN KEY ("+USER_ID+") REFERENCES "+TABLE_USER+" ("+USER_ID+") ON DELETE SET NULL DEFERRABLE);";
 
 
-    final private String[] CREATE_TABLES = {CREATE_BUILDING,CREATE_BUILDING_MAPS,CREATE_ROOM,CREATE_USER};
+    final private String[] CREATE_TABLES = {CREATE_BUILDING,CREATE_BUILDING_MAPS,CREATE_ROOM,CREATE_USER,CREATE_RESERVATION};
 
     public LocalUserHelper(Context context) {
         super(context, TABLE_USER, null, VERSION);
@@ -134,8 +148,27 @@ public class LocalUserHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        for(int x = TABLES.length-1; x >= 0; x --)
-            db.execSQL("DROP TABLE IF EXISTS "+TABLES[x]);
+        Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+
+        if (c.moveToFirst()) {
+            while ( !c.isAfterLast() ) {
+                System.out.println("TABLE NAME=> "+c.getString(0));
+                c.moveToNext();
+            }
+        }
+        for(int x = TABLES.length-1; x >= 0; x --) {
+            System.out.println("DROP TABLE IF EXISTS "+TABLES[x]);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLES[x]);
+        }
+        System.out.println("AFTER DROPS");
+        c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+
+        if (c.moveToFirst()) {
+            while ( !c.isAfterLast() ) {
+                System.out.println("TABLE NAME=> "+c.getString(0));
+                c.moveToNext();
+            }
+        }
       //  db.execSQL("DROP TABLE IF EXISTS dormbuddy_user");
       //  db.execSQL("DROP TABLE IF EXISTS "+TABLE_BUILDING);
       //  db.execSQL("DROP TABLE IF EXISTS "+TABLE_BUILDING_MAPS);
